b2facb981d3395c3ea5c1a4edda36572e1fdbe42
Jdwum4 <jdwum4@users.noreply.github.com>
2021-05-08 18:26:00 -0400

    Update crash-course.py

--- crash-course.py
+++ crash-course.py
@@ -2,61 +2,85 @@
 #### Crash-Course on Python
 
 """ 
-#Lesson:  Introduction to Python?
+##Lesson:  Introduction to Python?
 
 # Prints Hi to each friend in the list 
-# friends = ['Taylor', 'Alex', 'Pat', 'Eli']
-#  for friend in friends:
-#      print("Hi " + friend)
+friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+ for friend in friends:
+     print("Hi " + friend)
 
 # Quiz: Introduction to Python
-# print("My first Python program")
+print("My first Python program")
 
 # Prints Have a nice day
-# print("Have a nice day")
+print("Have a nice day")
 
 #Prints "This is fun!" onto the screen 5 times
-# for i in range(5):
-#      print("This is fun!")
+for i in range(5):
+     print("This is fun!")
 
 #Prints out number less than 10 
-# for i in range(10):
-#     print(i)
+for i in range(10):
+    print(i)
 
  """
 
-###Lesson: Hello World
+""" 
+##Lesson: Hello World
 
-# #Prints I'm programming in Python!
-# print("I'm programming in Python!")
+#Prints I'm programming in Python!
+print("I'm programming in Python!")
 
-# #Prints Hello to Brook
-# name= "Brook "
-# print("Hello "+name)
-# name= "Alex "
-# print("Hello "+name)
-# name
+#Prints Hello to Brook
+name= "Brook "
+print("Hello "+name)
+name= "Alex "
+print("Hello "+name)
+name
 
-# #Prints the color of cold
-# color = "Yellow"
-# thing = "Gold"
-# print(color + " is the color of " + thing)
+#Prints the color of cold
+color = "Yellow"
+thing = "Gold"
+print(color + " is the color of " + thing)
 
-# #Calculations
-# print(4+5)
-# print(9*7)
-# print(-1/4)
-# print(1/3)
-# print(((2050/5)-32)/9)
-# print(2**10)
+#Calculations
+print(4+5)
+print(9*7)
+print(-1/4)
+print(1/3)
+print(((2050/5)-32)/9)
+print(2**10)
 
 #Calculating with python
-# print((((1+2)*3)/4)**5)
+print((((1+2)*3)/4)**5)
 
 # Quiz: Hello World
 #Prints programming is fun
-# print("Programming in Python is fun!")
+print("Programming in Python is fun!")
 
 # #Calculates the Golden Ratio
-# ratio = (1+5**(1/2))/2
-# print(ratio)
+ratio = (1+5**(1/2))/2
+print(ratio)
+ """
+
+ ####### Graded Assement #######
+
+ #Prints Automatin with Python is fun
+print("Automating with Python is fun!")
+
+#Prints Yellow is the color of sunshine 
+color = "Yellow"
+thing = "sunshine"
+print(color + " is the color of " + thing)
+
+#Prints how many seconds there are in a week, if a week is 7 days.
+print((7/1)*86400)
+
+#Calculates te power of 26 to the power 6
+print((26**6))
+
+#Calculates the sector size of a 16GB hard disk with a sector size of 512 bytes.
+disk_size = 16*1024*1024*1024
+sector_size = 512
+sector_amount = disk_size/sector_size
+print(sector_amount)
