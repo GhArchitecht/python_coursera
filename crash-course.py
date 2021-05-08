0be460d1364a7e02d273b8ff10041b8dd2531a2b
Jdwum4 <jdwum4@users.noreply.github.com>
2021-05-08 16:45:41 -0400

    Create crash-course.py

---
+++ crash-course.py
@@ -0,0 +1,23 @@
+# Week 1 
+# Crash-Course on Python
+
+# Lesson:  Introduction to Python?
+
+# Prints Hi to each friend in the list 
+# friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+# for friend in friends:
+#     print("Hi " + friend)
+
+# Quiz: Introduction to Python
+# print("My first Python program")
+
+# #Prints Have a nice day
+# print("Have a nice day")
+
+#Prints "This is fun!" onto the screen 5 times
+# for i in range(5):
+#     print("This is fun!")
+
+#Prints out number less than 10 
+for i in range(10):
+    print(i)
