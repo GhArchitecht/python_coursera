3cbe570ed9fbdccdd42b28bbce0e603ec487b7f8
Jdwum4 <jdwum4@users.noreply.github.com>
2021-05-08 17:30:56 -0400

    Update crash-course.py

--- crash-course.py
+++ crash-course.py
@@ -1,23 +1,62 @@
-# Week 1 
-# Crash-Course on Python
+###### Week 1 
+#### Crash-Course on Python
 
-# Lesson:  Introduction to Python?
+""" 
+#Lesson:  Introduction to Python?
 
 # Prints Hi to each friend in the list 
 # friends = ['Taylor', 'Alex', 'Pat', 'Eli']
-# for friend in friends:
-#     print("Hi " + friend)
+#  for friend in friends:
+#      print("Hi " + friend)
 
 # Quiz: Introduction to Python
 # print("My first Python program")
 
-# #Prints Have a nice day
+# Prints Have a nice day
 # print("Have a nice day")
 
 #Prints "This is fun!" onto the screen 5 times
 # for i in range(5):
-#     print("This is fun!")
+#      print("This is fun!")
 
 #Prints out number less than 10 
 # for i in range(10):
 #     print(i)
+
+ """
+
+###Lesson: Hello World
+
+# #Prints I'm programming in Python!
+# print("I'm programming in Python!")
+
+# #Prints Hello to Brook
+# name= "Brook "
+# print("Hello "+name)
+# name= "Alex "
+# print("Hello "+name)
+# name
+
+# #Prints the color of cold
+# color = "Yellow"
+# thing = "Gold"
+# print(color + " is the color of " + thing)
+
+# #Calculations
+# print(4+5)
+# print(9*7)
+# print(-1/4)
+# print(1/3)
+# print(((2050/5)-32)/9)
+# print(2**10)
+
+#Calculating with python
+# print((((1+2)*3)/4)**5)
+
+# Quiz: Hello World
+#Prints programming is fun
+# print("Programming in Python is fun!")
+
+# #Calculates the Golden Ratio
+# ratio = (1+5**(1/2))/2
+# print(ratio)
